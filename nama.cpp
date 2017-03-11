b9790939e98fb18c0eee17930064cbac7f334cdc
YogiRahmatullah <ogirahmatullah@gmail.com>
2017-03-11 12:39:31 +0700

    Add files via upload

---
+++ nama.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+     char nama [25];
+	 
+	 cout << "Masukan Nama Anda : ";
+	 cin >> nama;
+	 cout << "Nama Anda Adalah : " << nama << endl;
+}	 
