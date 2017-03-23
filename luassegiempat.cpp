7bc6cddd93adbc7a60e3c2cfb932987b0fb856e6
YogiRahmatullah <ogirahmatullah@gmail.com>
2017-03-23 12:48:54 +0700

    Add files via upload

---
+++ luassegiempat.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+   int panjang;
+   int lebar;
+   cout << "Masukan Panjang :" ;
+   cin >> panjang ;
+   cout << "Masukan Lebar :" ;
+   cin >> lebar ;
+   cout << "Luas Segi Empat Adalah :" ;
+   cout << panjang*lebar ;
+   return 0 ;
+}   
